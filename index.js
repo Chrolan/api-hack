b06fdb3812965b3e487e938c709d70186b46ccf3
Chrolan <jordikleriga@gmail.com>
2018-08-01 15:55:29 -0500

    prelim code, nothing is working as of yet.

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const YouTubeURL = 'https://www.googleapis.com/youtube/v3/search';
+//APIs we will use in this website
+const diveAPI = 'http://api.divesites.com/';
+const geoLocationAPI = 'https://maps.googleapis.com/maps/api/geocode/json'
 
-//function for getting data and then setting up later call back function (later will be something you do with the data
-function getDataFromAPI (queryTerm,callback) {
+
+
+
+//function to get dive data
+function getDiveData (longitude,latitude,callback) {
     //create constant that will use passed through query
     const query = {
-        part: 'snippet',
-        id: '7lCDEYXw3mM',
-        key: 'AIzaSyAvrGLiEMNyCaOFnNPzTONXEGLi4b01JH4',
-        q: `${queryTerm}`,
-        maxResults: 5,
-        type: '',
+        mode: 'sites',
+        lng: longitude,
+        lat:latitude,
+        dist: 50
     };
     //call JSON method
-    $.getJSON(YouTubeURL,query,callback)
+    $.getJSON(diveAPI,query,callback)
 }
+
+
+
+
+function startWebsite() {
+    getDiveData();
+}
+
+
+$(startWebsite());
