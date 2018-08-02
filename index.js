9f9c905b7f42360f14d40f6e10ec0329e0a1bd9c
Chrolan <jordikleriga@gmail.com>
2018-08-02 13:12:21 -0500

    Got Search working and passing into google geoAPI

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 //APIs we will use in this website
 const diveAPI = 'http://api.divesites.com/';
-const geoLocationAPI = 'https://maps.googleapis.com/maps/api/geocode/json'
+const geoLocationAPI = 'https://maps.googleapis.com/maps/api/geocode/json';
 
+
+//function to get the search value used in input field, this will be passed to a geolocation API service to obtain its long/lat
+function getSerachParameter () {
+    $('.js-search').submit(event => {
+    event.preventDefault();
+    console.log($('.js-search-parameter').val());
+    let address = $('.js-search-parameter').val();
+    googleMapApi(address);
+    });
+}
 
 
 
@@ -18,11 +28,23 @@ function getDiveData (longitude,latitude,callback) {
     $.getJSON(diveAPI,query,callback)
 }
 
+//function to get the long/lat of the given search result
+function googleMapApi (location) {
+    //create constant that will use passed through query
+    const query = {
+        key: 'AIzaSyBp_stUCKfLCgsrrQjS8cda2oWFCP3t4EM',
+        address: location,
+    };
+    //call JSON method
+    console.log($.getJSON(geoLocationAPI,query));
+}
 
 
 
 function startWebsite() {
-    getDiveData();
+    //getDiveData();
+    getSerachParameter();
+
 }
 
 
